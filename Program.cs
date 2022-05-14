4d3316bb730295a98829c1c47e855632b5fce465
nirzaf <mfmfazrin1986@gmail.com>
2022-05-14 10:07:23 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+//WriteLine("Enter your name here");
+//var input = ReadLine();
+//WriteLine(input);
+
+
+int num1 = 10;
+int num2 = 20;
+
+
+
+WriteLine(num1+num2);
